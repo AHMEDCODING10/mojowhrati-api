f2c2fc3f243e4945df0cc547888cb952b8a59ed5
AHMEDCODING10 <ahmedtamis00@gmail.com>
2026-04-06 08:46:23 +0300

    Fix CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->prepend(\App\Http\Middleware\Cors::class);
         $middleware->validateCsrfTokens(except: [
             'broadcasting/auth',
         ]);
