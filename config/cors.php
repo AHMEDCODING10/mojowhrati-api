37bfac0d2629e3e42ae5640d0bbfe93cdc95a497
AHMEDCODING10 <ahmedtamis00@gmail.com>
2026-04-06 08:40:57 +0300

    Fix CORS policy for Flutter Web

---
+++ config/cors.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'], // Allow all methods (GET, POST, etc.)
+
+    'allowed_origins' => ['*'], // Allow all origins
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'], // Allow all headers
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
