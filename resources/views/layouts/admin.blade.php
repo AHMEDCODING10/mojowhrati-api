4c38e50ad3b8ef9211327ddcb1ce47c0b8d064e3
AHMEDCODING10 <ahmedtamis00@gmail.com>
2026-04-17 19:23:46 +0300

    Fix: Propagate CSP change by moving tag to top and adding cache buster

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,11 +6,12 @@
 }" :class="darkMode ? 'dark' : ''">
 
 <head>
+    <!-- System Version 1.0.1-Reset -->
     <meta charset="utf-8">
+    <meta http-equiv="Content-Security-Policy"
+        content="default-src * 'self' 'unsafe-inline' 'unsafe-eval' data: gap: content: blob:">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="Content-Security-Policy"
-        content="default-src * 'self' 'unsafe-inline' 'unsafe-eval' data: gap: content:">
 
     <title>{{ config('app.name', 'Mojawharati') }}</title>
 
